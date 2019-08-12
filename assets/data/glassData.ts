f14d56163f201c0a104c8bebcfa99fb3527c61fe
JainSheridan <50390799+JainSheridan@users.noreply.github.com>
2019-08-12 11:57:58 -0400

    Add files via upload

---
+++ assets/data/glassData.ts
@@ -0,0 +1,7 @@
+import {Glass} from '../../app/Glass';
+
+export const info : Glass[]=[
+    {type:'Gorilla Glass',make:'Apple',country:'India'},
+    {type:'Tempered Soda Lime Glass',make:'Samsung',country:'America'},
+    {type:'Borosilicate Glass',make:'Blackberry',country:'Canada'},
+];
